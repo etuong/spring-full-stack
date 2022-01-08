4f29558f8835c1bb81363615716e7afb96f32ef8
etuong <etuong@gmail.com>
2022-01-08 10:16:31 -0800

    Thyme leaf

--- src/main/java/com/meetup/MeetupApplication.java
+++ src/main/java/com/meetup/MeetupApplication.java
@@ -1,24 +1,11 @@
 package com.meetup;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MeetupApplication extends SpringBootServletInitializer implements CommandLineRunner {
-
-    @Autowired
-    private Seeder seeder;
-
+public class MeetupApplication {
     public static void main(String[] args) {
         SpringApplication.run(MeetupApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        seeder.seedUsers();
-        seeder.seedBooks();
     }
 }
